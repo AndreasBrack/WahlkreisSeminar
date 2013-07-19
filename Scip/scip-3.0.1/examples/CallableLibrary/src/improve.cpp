cbfc9efbd635de86c8632cdbb6f092c658372860
AndreasBrack <andreas.brack@rwth-aachen.de>
2013-07-19 09:01:48 +0200

    Innitial of Improving Heuristic

---
+++ Scip/scip-3.0.1/examples/CallableLibrary/src/improve.cpp
@@ -0,0 +1,97 @@
+/*
+ * improve.cpp
+ *
+ *  Created on: 24.06.2013
+ *      Author: andreas
+ */
+
+//#define SCIP_STATISTIC
+
+#include "improve.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <math.h>
+
+#include "objscip/objscip.h"
+#include "scip/cons_linear.h"
+#include "ConshdlrSubtree.h"
+#include "ReaderWP.h"
+#include "ProbDataWP.h"
+
+using namespace tree;
+using namespace scip;
+using namespace std;
+
+
+
+/** destructor of primal heuristic to free user data (called when SCIP is exiting) */
+SCIP_DECL_HEURFREE(heur_voronoi::scip_free)
+{
+	return SCIP_OKAY;
+}
+
+/** initialization method of primal heuristic (called after problem was transformed) */
+SCIP_DECL_HEURINIT(heur_voronoi::scip_init)
+{
+	ProbDataWP*   probdata;
+
+	/* load the problem specific data */
+	probdata = dynamic_cast<ProbDataWP*>(SCIPgetObjProbData(scip));
+	assert(probdata != NULL);
+
+	graph = probdata->getGraph();
+	assert(graph != NULL);
+
+	capture_graph(graph);
+
+	return SCIP_OKAY;
+}
+
+/** deinitialization method of primal heuristic (called before transformed problem is freed) */
+SCIP_DECL_HEUREXIT(heur_voronoi::scip_exit)
+{
+	/* free everything which was created in scip_init */
+	release_graph(scip, &graph);
+
+	return SCIP_OKAY;
+}
+
+/** solving process initialization method of primal heuristic (called when branch and bound process is about to begin)
+ *
+ *  This method is called when the presolving was finished and the branch and bound process is about to begin.
+ *  The primal heuristic may use this call to initialize its branch and bound specific data.
+ *
+ */
+SCIP_DECL_HEURINITSOL(heur_voronoi::scip_initsol)
+{
+	return SCIP_OKAY;
+}
+
+/** solving process deinitialization method of primal heuristic (called before branch and bound process data is freed)
+ *
+ *  This method is called before the branch and bound process is freed.
+ *  The primal heuristic should use this call to clean up its branch and bound data.
+ */
+SCIP_DECL_HEUREXITSOL(heur_voronoi::scip_exitsol)
+{
+	return SCIP_OKAY;
+}
+
+
+/** execution method of primal heuristic */
+SCIP_DECL_HEUREXEC(heur_voronoi::scip_exec)
+{  /*lint --e{715}*/
+
+
+	return SCIP_OKAY;
+}
+
+/** clone method which will be used to copy a objective plugin */
+SCIP_DECL_HEURCLONE(scip::ObjCloneable* heur_voronoi::clone)
+{
+	return new heur_voronoi(scip);
+}
---
+++ Scip/scip-3.0.1/examples/CallableLibrary/src/improve.h
@@ -0,0 +1,99 @@
+/*
+ * heurvoronoi.h
+ *
+ *  Created on: 24.06.2013
+ *      Author: andreas
+ */
+
+// #define SCIP_DEBUG
+/*---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include <vector>
+
+#include "objscip/objscip.h"
+
+#include "scip/cons_linear.h"
+#include <math.h>
+
+#include "ConshdlrSubtree.h"
+#include "ReaderWP.h"
+#include "ProbDataWP.h"
+
+using namespace tree;
+using namespace scip;
+using namespace std;
+
+#ifndef IMPROVE_H_
+#define IMPROVE_H_
+
+namespace tree {
+
+class heur_improve: public scip::ObjHeur {
+	GRAPH* graph; /**< the underlying graph of the TSP */
+	SCIP_SOL* sol; /**< current solution */
+
+public:
+	/** default constructor */
+	heur_improve(SCIP* scip) :
+			ObjHeur(scip, "improve", "Improve", 'I', 50000, 1000, 0, 0,
+					SCIP_HEURTIMING_AFTERNODE, FALSE) {
+	}
+
+	/** destructor */
+	virtual ~heur_improve() {
+	}
+
+	/** destructor of primal heuristic to free user data (called when SCIP is exiting) */
+	virtual SCIP_DECL_HEURFREE(scip_free);
+
+	/** initialization method of primal heuristic (called after problem was transformed) */
+	virtual SCIP_DECL_HEURINIT(scip_init);
+
+	/** deinitialization method of primal heuristic (called before transformed problem is freed) */
+	virtual SCIP_DECL_HEUREXIT(scip_exit);
+
+	virtual SCIP_DECL_HEURINITSOL(scip_initsol);
+
+	/** solving process deinitialization method of primal heuristic (called before branch and bound process data is freed)
+	 *
+	 *  This method is called before the branch and bound process is freed.
+	 *  The primal heuristic should use this call to clean up its branch and bound data.
+	 */
+	virtual SCIP_DECL_HEUREXITSOL(scip_exitsol);
+
+	/** execution method of primal heuristic
+	 *
+	 *  Searches for feasible primal solutions. The method is called in the node processing loop.
+	 *
+	 *  possible return values for *result:
+	 *  - SCIP_FOUNDSOL   : at least one feasible primal solution was found
+	 *  - SCIP_DIDNOTFIND : the heuristic searched, but did not find a feasible solution
+	 *  - SCIP_DIDNOTRUN  : the heuristic was skipped
+	 *  - SCIP_DELAYED    : the heuristic was skipped, but should be called again as soon as possible, disregarding
+	 *                      its frequency
+	 */
+	virtual SCIP_DECL_HEUREXEC(scip_exec);
+
+	/** clone method which will be used to copy a objective plugin */
+	virtual SCIP_DECL_HEURCLONE(ObjCloneable* clone);
+
+	/** returns whether the objective plugin is copyable */
+	virtual SCIP_DECL_HEURISCLONEABLE(iscloneable) {
+		return TRUE;
+	}
+};
+
+}
+
+class improve {
+public:
+	virtual ~improve();
+	improve();
+};
+#endif /* HEURVORONOI_H_ */
+
